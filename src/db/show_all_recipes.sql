2974fdafa433376b3bc8429eb2fb79e0b8bd4be1
Mouse <matthewg42@gmail.com>
2014-07-06 19:01:46 +0100

    consistent abbreviations between queries

--- src/db/show_all_recipes.sql
+++ src/db/show_all_recipes.sql
@@ -1,9 +1,9 @@
-SELECT 		r.name, ri.seq + '.', i.name, ri.qty * d.unit_size, d.unit_name
+SELECT 		r.name, ri.seq + '.', i.name, ri.qty * dt.unit_size, dt.unit_name
 FROM 		recipe_ingredient ri, 
 		recipe r, 
 		ingredient i, 
-		dispenser_type d
+		dispenser_type dt
 WHERE 		r.id = ri.recipe_id
 AND 		ri.ingredient_id = i.id
-AND 		i.dispenser_type_id = d.id
+AND 		i.dispenser_type_id = dt.id
 ORDER BY 	r.id, ri.seq;
