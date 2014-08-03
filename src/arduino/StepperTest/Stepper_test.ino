5ed84f893e9bdfe4800f624bf5144de6edc00728
Mouse <matthewg42@gmail.com>
2014-08-04 00:04:15 +0100

    make Stepper test buildable using make (add libs, add fn prototypes)

--- src/arduino/StepperTest/Stepper_test.ino
+++ src/arduino/StepperTest/Stepper_test.ino
@@ -34,6 +34,10 @@ Servo servoMixer3;
 Servo servoMixer4; 
 Servo servoMixer5; 
 
+void colorWipe(uint32_t c, uint8_t wait);
+void rainbowCycle(uint8_t wait);
+uint32_t Wheel(byte WheelPos);
+
 void setup()
 {  
   stepper.setMaxSpeed(1000);
