014257dbc74b00f958ac84180d502422aec8d656
Daniel <hs@dswann.co.uk>
2014-08-06 19:01:49 +0100

    update positions in db

--- src/db/test_data.sql
+++ src/db/test_data.sql
@@ -28,37 +28,37 @@ INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SEL
 INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Lemon juice', id, 1, 0 FROM dispenser_type WHERE name = 'Dasher';
 INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Lime juice', id, 1, 0 FROM dispenser_type WHERE name = 'Dasher';
 INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Angorstura bitters', id, 1, 1 FROM dispenser_type WHERE name = 'Dasher';
-INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Grenadine', id, 1000, 1 FROM dispenser_type WHERE name = 'Syringe';
-INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Cherry', id, 1, 1 FROM dispenser_type WHERE name = 'Conveyor';
-INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Lemon', id, 1, 1 FROM dispenser_type WHERE name = 'Slice dispenser';
-INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Lime', id, 1, 1 FROM dispenser_type WHERE name = 'Slice dispenser';
-INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Good stir', id, 1, 1 FROM dispenser_type WHERE name = 'Stirrer';
-INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Umbrella', id, 1, 1 FROM dispenser_type WHERE name = 'Umbrella dropper';
+INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Grenadine', id, 1000, 0 FROM dispenser_type WHERE name = 'Syringe';
+INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Cherry', id, 1, 0 FROM dispenser_type WHERE name = 'Conveyor';
+INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Lemon', id, 1, 0 FROM dispenser_type WHERE name = 'Slice dispenser';
+INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Lime', id, 1, 0 FROM dispenser_type WHERE name = 'Slice dispenser';
+INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Good stir', id, 1, 0 FROM dispenser_type WHERE name = 'Stirrer';
+INSERT INTO ingredient (name, dispenser_type_id, dispenser_param, alcoholic) SELECT 'Umbrella', id, 1, 0 FROM dispenser_type WHERE name = 'Umbrella dropper';
 
 -- Ingredients we have loaded into dispensers (also dispenser configs)
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 0',     10 FROM ingredient WHERE name = 'Vodka';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 1',     20 FROM ingredient WHERE name = 'Whisky';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 2',     30 FROM ingredient WHERE name = 'Vodka';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 3',     40 FROM ingredient WHERE name = 'Gin';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 4',     50 FROM ingredient WHERE name = 'Tequila';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 5',     60 FROM ingredient WHERE name = 'Rum';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 0',             0 FROM ingredient WHERE name = 'Vodka';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 1',           546 FROM ingredient WHERE name = 'Whisky';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 2',          1093 FROM ingredient WHERE name = 'Vodka';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 3',          1640 FROM ingredient WHERE name = 'Gin';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 4',          2187 FROM ingredient WHERE name = 'Tequila';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Optic 5',          2734 FROM ingredient WHERE name = 'Rum';
 
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 0',  70 FROM ingredient WHERE name = 'Ginger ale';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 1',  76 FROM ingredient WHERE name = 'Coke';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 2',  82 FROM ingredient WHERE name = 'Lemonade';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 3',  88 FROM ingredient WHERE name = 'Tonic water';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 4',  94 FROM ingredient WHERE name = 'Soda water';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 5', 100 FROM ingredient WHERE name = 'Orange juice';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 0',       3117 FROM ingredient WHERE name = 'Ginger ale';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 1',       3417 FROM ingredient WHERE name = 'Coke';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 2',       3746 FROM ingredient WHERE name = 'Lemonade';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 3',       4057 FROM ingredient WHERE name = 'Tonic water';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 4',       4357 FROM ingredient WHERE name = 'Soda water';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Pressure 5',       4714 FROM ingredient WHERE name = 'Orange juice';
 
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Dasher 0', 106 FROM ingredient WHERE name = 'Angorstura bitters';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Dasher 1', 112 FROM ingredient WHERE name = 'Lemon juice';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Dasher 2', 118 FROM ingredient WHERE name = 'Tabasco';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Dasher 0',         5058 FROM ingredient WHERE name = 'Angorstura bitters';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Dasher 1',         5332 FROM ingredient WHERE name = 'Lemon juice';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Dasher 2',         5605 FROM ingredient WHERE name = 'Tabasco';
 
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Syringe', 122 FROM ingredient WHERE name = 'Grenadine';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Conveyor', 126 FROM ingredient WHERE name = 'Cherry';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Slice dispenser', 136 FROM ingredient WHERE name = 'Lime';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Stirrer dispenser', 142 FROM ingredient WHERE name = 'Good stir';
-INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Umbrella dropper', 146 FROM ingredient WHERE name = 'Umbrella';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Syringe' ,         5800 FROM ingredient WHERE name = 'Grenadine';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Conveyor',         5900 FROM ingredient WHERE name = 'Cherry';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Slice dispenser',  6335 FROM ingredient WHERE name = 'Lime';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Stirrer dispenser',6792 FROM ingredient WHERE name = 'Good stir';
+INSERT INTO dispenser (dispenser_type_id, ingredient_id, name, rail_position) SELECT dispenser_type_id, id, 'Umbrella dropper', 7002 FROM ingredient WHERE name = 'Umbrella';
 
 -- Cocktail recipes!
 INSERT INTO recipe (name) VALUES ('Gin and tonic (lemon lime)');
